357d8a5916c345555dd6a741e0f00a546748c1dd
Nayara <nayara.silva.s@hotmail.com>
2022-04-07 14:13:02 +0000

    infra(chore): adiciona views

--- dist/index.js
+++ dist/index.js
@@ -4,11 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const port = 3000; // default port to listen
+app.set("views", path_1.default.join(__dirname, "views"));
+app.set("view engine", "twig");
 // define a route handler for the default home page
 app.get("/", (req, res) => {
-    res.send("Hello world!");
+    res.render("cabecalho.html.twig");
 });
 // start the Express server
 app.listen(port, () => {
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,19 @@
-import express, {Request,Response} from "express";
+import express, { Request, Response } from "express";
+import path from "path";
 
 const app = express();
 const port = 3000; // default port to listen
 
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "twig");
+
 // define a route handler for the default home page
-app.get( "/", ( req:Request, res:Response ) => {
-    res.send( "Hello world!" );
+app.get("/", (req: Request, res: Response) => {
+  res.render("cabecalho.html.twig");
 });
 
 // start the Express server
-app.listen( port, () => {
-    // tslint:disable-next-line: no-console
-    console.log( `server started at \x1b[36mhttp://localhost:${ port }\x1b[0m` );
+app.listen(port, () => {
+  // tslint:disable-next-line: no-console
+  console.log(`server started at \x1b[36mhttp://localhost:${port}\x1b[0m`);
 });
