97b3f2241f952f0ca25aed264b7f19f8340c462a
Nayara <nayara.silva.s@hotmail.com>
2022-04-08 13:14:23 +0000

    feat(backend): adiciona action post /usuarios adiciona novo usuario

--- dist/controllers/UsuariosController.js
+++ dist/controllers/UsuariosController.js
@@ -1,13 +1,37 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.adiciona = void 0;
 const express_validator_1 = require("express-validator");
-function adiciona(req, res) {
+const sequelize_1 = require("sequelize");
+const Usuario_1 = __importDefault(require("../models/Usuario"));
+async function adiciona(req, res) {
     const errors = (0, express_validator_1.validationResult)(req);
     if (!errors.isEmpty()) {
-        return res.status(422).json({ errors: errors.array() });
+        return res.status(424).json({ errors: errors.array() });
     }
-    res.send("Olá Mundo");
+    try {
+        const used = await Usuario_1.default.findOne({
+            where: {
+                [sequelize_1.Op.and]: [{ email: req.body.email }],
+            },
+        });
+        if (used) {
+            return res.status(422).send({ error: "Email já em uso." });
+        }
+        const novoUsuario = await Usuario_1.default.create(req.body);
+        return res.status(200).json({
+            id: novoUsuario.id,
+            nome: novoUsuario.nome,
+            sobrenome: novoUsuario.sobrenome,
+            email: novoUsuario.email,
+        });
+    }
+    catch (error) {
+        return res.status(422).send({ error: "Houve um error." });
+    }
 }
 exports.adiciona = adiciona;
 //# sourceMappingURL=UsuariosController.js.map
--- src/controllers/UsuariosController.ts
+++ src/controllers/UsuariosController.ts
@@ -1,11 +1,34 @@
 import { validationResult, Result } from "express-validator";
 import { Request, Response } from "express";
+import { Op } from "sequelize";
+import Usuario from "../models/Usuario";
 
-export function adiciona(req: Request, res: Response) {
+export async function adiciona(req: Request, res: Response) {
   const errors: Result = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({ errors: errors.array() });
+    return res.status(424).json({ errors: errors.array() });
   }
 
-  res.send("Olá Mundo");
+  try {
+    const used = await Usuario.findOne({
+      where: {
+        [Op.and]: [{ email: req.body.email }],
+      },
+    });
+
+    if (used) {
+      return res.status(422).send({ error: "Email já em uso." });
+    }
+
+    const novoUsuario = await Usuario.create(req.body);
+
+    return res.status(200).json({
+      id: novoUsuario.id,
+      nome: novoUsuario.nome,
+      sobrenome: novoUsuario.sobrenome,
+      email: novoUsuario.email,
+    });
+  } catch (error) {
+    return res.status(422).send({ error: "Houve um error." });
+  }
 }
