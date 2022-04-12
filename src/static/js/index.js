b29bec46b6f5eafa398ba6c623d7812ea4bfa520
Nayara <nayara.silva.s@hotmail.com>
2022-04-12 11:23:10 +0000

    feat(frontend): adiciona botao resetar ou limpar

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,5 +1,9 @@
 import paginate from "./paginate.js";
 
 const buscar = document.querySelector(".buscar");
-
 buscar.addEventListener("click", paginate);
+
+const limpar = document.querySelector(".limpar");
+limpar.addEventListener("click", function () {
+  document.getElementById("pesquisa").reset();
+});
